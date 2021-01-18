8a7679c351172020e7457a69171c51279c76ed59
JungMin LEE <leejungmin05@gmail.com>
2021-01-19 03:39:51 +0900

    Add photo function & database

--- app/src/main/java/com/example/cloneinstagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/navigation/AddPhotoActivity.kt
@@ -1,17 +1,23 @@
 package com.example.cloneinstagram.navigation
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cloneinstagram.R
+import com.example.cloneinstagram.navigation.model.ContentDTO
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,14 +25,20 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    var auth : FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
+
     private lateinit var addphoto_btn_upload : Button
     private lateinit var addphoto_image : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
-        //initate storage
+        //initiate storage
         storage = FirebaseStorage.getInstance()
+        auth= FirebaseAuth.getInstance()
+        firestore= FirebaseFirestore.getInstance()
 
         //Open the album
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -40,32 +52,79 @@ class AddPhotoActivity : AppCompatActivity() {
         addphoto_btn_upload=findViewById(R.id.addphoto_btn_upload)
     }
 
+    class ActivityResultSampleActivity : AppCompatActivity() {
+        val requestLocation: ActivityResultLauncher<String> = registerForActivityResult(
+                RequestPermission(), ACCESS_FINE_LOCATION
+        ) {isGranted->
 
-    override fun PreferenceManager.OnActivityResultListener(requestCode :Int, resultCode: Int, data: Intent?) {
-       super.onActivityResult(requestCode, resultCode, data)
-       if(requestCode == PICK_IMAGE_FROM_ALBUM){
-           if(resultCode == Activity.RESULT_OK){
-               //this is path to the selected image
-               photoUri = data?.data
-               addphoto_image.setImageURI(photoUri)
-               addphoto_image=findViewById(R.id.addphoto_image)
-           }else {
-               //Exit the addPhotoActivity if you leave the album without selecting it
-               finish()
-               }
-       }
+        }
+
+        private fun requestLocationAction(){
+            requestLocation()
+        }
     }
+
     fun contentUpload() {
         //Make filename
 
         var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         var imageFileName ="IMAGE_" + timestamp + "_.png"
-
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
-        //FileUpload
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        //promise method (구글 권장 방식)
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task : Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+            //Insert dowmloadUrl of image
+            contentDTO.imageUrl = uri.toString()
+
+            //Insert uid of user
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //Insert userID
+            contentDTO.userId = auth?.currentUser?.email
+
+            //Insert explain of content
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            //Inser timestamp
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
         }
+
+
+
+      /*  //Callback method
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var contentDTO = ContentDTO()
+                //Insert dowmloadUrl of image
+                contentDTO.imageUrl = uri.toString()
+
+                //Insert uid of user
+                contentDTO.uid = auth?.currentUser?.uid
+
+                //Insert userID
+                contentDTO.userId = auth?.currentUser?.email
+
+                //Insert explain of content
+                contentDTO.explain = addphoto_edit_explain.text.toString()
+
+                //Inser timestamp
+                contentDTO.timestamp = System.currentTimeMillis()
+
+                firestore?.collection("images")?.document()?.set(contentDTO)
+
+                setResult(Activity.RESULT_OK)
+
+                finish()
+            }
+        }*/
     }
 }
---
+++ app/src/main/java/com/example/cloneinstagram/navigation/model/ContentDTO.kt
@@ -0,0 +1,15 @@
+package com.example.cloneinstagram.navigation.model
+
+data class ContentDTO   (var explain : String? = null, // 컨텐츠 설명 관리
+                         var imageUrl : String? = null, // 이미지 주소 관리
+                         var uid : String? = null, // 어느 유저가 올렸는지
+                         var userId : String? = null, //올린 유저의 이미지
+                         var timestamp : Long? = null, //컨텐츠 시간
+                         var favoriteCount : Int = 0, // 좋아요 갯수 //좋아요 유저 관리
+                         var favorties : Map<String,Boolean> = HashMap()) {
+    data class  Comment(var uid: String? = null,
+                        var userId : String? = null,
+                        var comment: String? = null,
+                        var timestamp: Long? = null)
+
+}
