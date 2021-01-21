d4591067832e189d20e352880e8c4ea5b4370b3b
JungMin LEE <leejungmin05@gmail.com>
2021-01-22 03:05:54 +0900

    Add userpage

--- app/src/main/java/com/example/cloneinstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/navigation/UserFragment.kt
@@ -5,13 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cloneinstagram.R
+import com.example.cloneinstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class UserFragment : Fragment(){
+    var fragmentView : View? = null
+    var firestore: FirebaseFirestore?= null
+    var uid : String?= null
+    var auth : FirebaseAuth?= null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
-        return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        return fragmentView
     }
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
 
+        init {
+            firestore?.collection("images")?.whereEqualTo("uid", uid)?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                if (querySnapshot == null) return@addSnapshotListener
+
+                //get data
+                for (snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+    }
 }
