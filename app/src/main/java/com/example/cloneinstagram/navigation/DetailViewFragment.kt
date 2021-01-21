cd19b9e3c3b93d4232b0cb05c9c3252b4a0488d8
JungMin LEE <leejungmin05@gmail.com>
2021-01-22 03:06:12 +0900

    Add userpage

--- app/src/main/java/com/example/cloneinstagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/navigation/DetailViewFragment.kt
@@ -6,18 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cloneinstagram.R
 import com.example.cloneinstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+
+
 
 class DetailViewFragment : Fragment(){
     var firestore : FirebaseFirestore?= null
-    private lateinit var detailviewitem_profile_textview : TextView
+    var uid : String? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)4
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
+
+        view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
+        view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
 
         return view
     }
@@ -39,22 +48,71 @@ class DetailViewFragment : Fragment(){
             }
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail,parent,false)
-            return CustomviewHolder(view)
-        }
+       override fun onCreateViewHolder(p0: ViewGroup, p1 : Int) : RecyclerView.ViewHolder{
+               var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail,p0,false)
+               return CustomViewHolder(view)
+           }
 
-        inner class CustomviewHolder(view: View) : RecyclerView.ViewHolder(view)  //메모리를 적게 사용하기 위해서
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)  //메모리를 적게 사용하기 위해서
 
         override fun getItemCount(): Int {
            return contentDTOs.size
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var viewHolder = (holder as CustomviewHolder).itemView
+            var viewholder = (holder as CustomViewHolder).itemView
 
             //UserID
-            viewHolder.detailviewitem_profile_textview.text = contentDTOs!![position].userId
+            viewholder.detailviewitem_profile_textview.text = contentDTOs!!.userId
+
+            //Image
+            Glide.with(holder.itemView.context).load(contentDTOs!![holder].imageUrl).into(viewholder.detailviewitem_imageview_content)
+
+            //Explain of content
+            viewholder.detailviewitem_explain_textview.text =contentDTOs!![holder].explain
+
+            //likes
+            viewholder.detailviewitem_favoritecounter_textview.text = "Likes" + contentDTOs!![holder].favoriteCount
+
+            //profileImage
+            Glide.with(holder.itemView.context).load(contentDTOs!![holder].imageUrl).into(viewholder.detailviewitem_profile_image)
+
+            //this code is when the button is clicked
+            viewholder.detailviewitem_favorite_imageview.setOnClickListner{
+                favoriteEvent(position)
+            }
+
+            //this code is when the page is loaded
+            if(contentDTOs!![position].favorites.containsKey(uid)){
+                //this is like status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            }else{
+                //this is unlike status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+
+        fun favoriteEvent(position: Int){
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if(contentDTO!!.favorties.containsKey(uid)){
+                    //when the button is clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount -1
+                    contentDTO?.favorties.remove(uid)
+
+                }else {
+                    //when the button is not clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount +1
+                    contentDTO?.favorties[uid!!] = true
+                }
+                transaction.set(tsDoc,contentDTO)
+
+            }
         }
     }
 }
