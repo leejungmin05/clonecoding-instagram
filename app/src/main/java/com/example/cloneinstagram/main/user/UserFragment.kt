3e23a29a4c240ad4ef5617b2c593b3870ac6d84b
JungMin LEE <leejungmin05@gmail.com>
2021-01-27 01:30:58 +0900

    Add Userpage

--- app/src/main/java/com/example/cloneinstagram/main/user/UserFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/main/user/UserFragment.kt
@@ -5,16 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.appcompat.widget.LinearLayoutCompat.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.cloneinstagram.R
 import com.example.cloneinstagram.main.home.DetailViewFragment
-import com.example.cloneinstagram.main.user.UserFragment
 import com.example.cloneinstagram.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_user.*
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 
 
@@ -27,6 +30,12 @@ class UserFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        uid = arguments?.getString("destinationUid")
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        fragmentView?.account_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.account_recyclerview?.layoutManager = GridLayoutManager(activity,3)
         return fragmentView
     }
     inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -49,16 +58,20 @@ class UserFragment : Fragment(){
            var width = resources.displayMetrics.widthPixels /3
 
             var imageview = ImageView(parent.context)
-            imageview.layoutParams = LayoutParams(width,width)
-            return UserFragmentRecyclerViewAdapter().CustomViewHolder(imageview)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return CustomViewHolder(imageview)
+        }
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview){
+
         }
 
         override fun getItemCount(): Int {
-
+            return contentDTOs.size
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+            var imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageview)
         }
     }
 }
