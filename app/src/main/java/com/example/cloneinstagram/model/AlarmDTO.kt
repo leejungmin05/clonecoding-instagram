cf3483a05d639c906aae9389ab28cdad05dc5e76
JungMin LEE <leejungmin05@gmail.com>
2021-01-28 19:50:52 +0900

    Add alarmevent

--- app/src/main/java/com/example/cloneinstagram/model/AlarmDTO.kt
+++ app/src/main/java/com/example/cloneinstagram/model/AlarmDTO.kt
@@ -1,10 +1,10 @@
 package com.example.cloneinstagram.model
 
-data class  AlarmDTO {
+data class  AlarmDTO (
     var destinationUid : String? = null,
     var userId : String? = null,
     var uid : String? = null,
     var kind : Int? = null,
     var message : String? = null,
     var timestamp : Long? = null
-}
+)
