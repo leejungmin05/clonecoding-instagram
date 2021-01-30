0b7fb793695e9a3a41adeea6ae5a119240fcba9b
JungMin LEE <leejungmin05@gmail.com>
2021-01-31 00:26:23 +0900

    Add followfunc

--- app/src/main/java/com/example/cloneinstagram/main/MainActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cloneinstagram.main
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +16,12 @@ import com.example.cloneinstagram.main.alarm.*
 import com.example.cloneinstagram.main.grid.GridFragment
 import com.example.cloneinstagram.main.upload.AddPhotoActivity
 import com.example.cloneinstagram.main.user.UserFragment
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -90,5 +96,22 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             R.id.action_home
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == UserFragment.PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK){
+            var imageUri = data?.data
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+            var storageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
+            storageRef.putFile(imageUri!!).continueWithTask { task:Task<UploadTask.TaskSnapshot> ->
+                    return@continueWithTask storageRef.downloadUrl
+
+            }.addOnSuccessListener { uri ->
+                var map = HashMap<String, Any>()
+                map["images"]= uri.toString()
+                FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/cloneinstagram/main/user/UserFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/main/user/UserFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cloneinstagram.main.user
 
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.appcompat.widget.LinearLayoutCompat.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +21,7 @@ import com.example.cloneinstagram.main.MainActivity
 import com.example.cloneinstagram.main.home.DetailViewFragment
 import com.example.cloneinstagram.model.AlarmDTO
 import com.example.cloneinstagram.model.ContentDTO
+import com.example.cloneinstagram.model.FollowDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,22 +37,27 @@ class UserFragment : Fragment(){
     var auth : FirebaseAuth?= null
     var currentUserUid :String? = null
 
+    companion object {
+        var PICK_PROFILE_FROM_ALBUM = 10
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        fragmentView =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
         uid = arguments?.getString("destinationUid")
         firestore = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
         currentUserUid = auth?.currentUser?.uid
 
-        if(uid==currentUserUid) {
+        if (uid == currentUserUid) {
             //Mypage
             fragmentView?.account_btn_follow_signout?.text = getString(R.string.signout)
-            fragmentView?.account_btn_follow_signout?.setOnClickListener{
+            fragmentView?.account_btn_follow_signout?.setOnClickListener {
                 activity?.finish()
-                startActivity(Intent(activity,LoginActivity::class.java))
+                startActivity(Intent(activity, LoginActivity::class.java))
                 auth?.signOut()
             }
-        }else {
+        } else {
             //otherUser
             fragmentView?.account_btn_follow_signout?.text = getString(R.string.follow)
             var mainActivity = (activity as MainActivity)
@@ -60,11 +68,119 @@ class UserFragment : Fragment(){
             mainActivity?.toolbar_title_image?.visibility = View.GONE
             mainActivity?.toolbar_username?.visibility = View.VISIBLE
             mainActivity?.toolbar_btn_back?.visibility = View.VISIBLE
+            fragmentView?.account_btn_follow_signout?.setOnClickListener {
+                requestFollow()
+            }
         }
-        fragmentView?.account_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
-        fragmentView?.account_recyclerview?.layoutManager = GridLayoutManager(activity,3)
-        return fragmentView
+            fragmentView?.account_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+            fragmentView?.account_recyclerview?.layoutManager = GridLayoutManager(activity, 3)
+
+            fragmentView?.account_iv_profile?.setOnClickListener {
+                var photoPickerIntent = Intent(Intent.ACTION_PICK)
+                photoPickerIntent.type = "image/*"
+                activity?.startActivityForResult(photoPickerIntent, PICK_PROFILE_FROM_ALBUM)
+            }
+            getProfileImage()
+            getFollowerAndFollowing()
+            return fragmentView
+        }
+
+        fun getFollowerAndFollowing() {
+            firestore?.collection("users")?.document(uid!!)
+                ?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                    if (documentSnapshot == null) return@addSnapshotListener
+                    var followDTO = documentSnapshot.toObject(FollowDTO::class.java)
+                    if (followDTO?.followingCount != null) {
+                        fragmentView?.account_tv_following_count?.text =
+                            followDTO?.followingCount?.toString()
+                    }
+                    if (followDTO?.followerCount != null) {
+                        fragmentView?.account_tv_follower_count?.text =
+                            followDTO?.followerCount?.toString()
+                        if (followDTO?.followers?.containsKey(currentUserUid!!)) {
+                            fragmentView?.account_btn_follow_signout?.text =
+                                getString(R.string.follow_cancel)
+                            fragmentView?.account_btn_follow_signout?.background?.setColorFilter(
+                                ContextCompat.getColor(requireActivity(), R.color.colorLightGray),
+                                PorterDuff.Mode.MULTIPLY
+                            )
+                        } else {
+
+                            if (uid != currentUserUid) {
+                                fragmentView?.account_btn_follow_signout?.text =
+                                    getString(R.string.follow)
+                                fragmentView?.account_btn_follow_signout?.background?.colorFilter =
+                                    null
+                            }
+                        }
+                    }
+         }
+        }
+
+
+
+    fun requestFollow() {
+        //save data to my account
+        var tsDocFollowing = firestore?.collection("users")?.document(currentUserUid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing!!).toObject(FollowDTO::class.java)
+            if(followDTO ==null) {
+                followDTO = FollowDTO()
+                followDTO!!.followingCount = 1
+                followDTO!!.followers[uid!!] =true
+
+                transaction.set(tsDocFollowing,followDTO)
+                return@runTransaction
+            }
+            if(followDTO.followings.containsKey(uid)){
+                //it remoe following third person when a third person follow me
+                followDTO?.followingCount = followDTO?.followingCount -1
+                followDTO?.followings?.remove(uid)
+            }else {
+                //it add following third person when a third person do not follow me
+                followDTO?.followingCount = followDTO?.followingCount + 1
+                followDTO?.followings[uid!!] = true
+
+            }
+            transaction.set(tsDocFollowing,followDTO)
+            return@runTransaction
+        }
+        //save data to third account
+        var tsDocFollower = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollower!!).toObject(FollowDTO::class.java)
+            if(followDTO ==null) {
+                followDTO = FollowDTO()
+                followDTO!!.followerCount = 1
+                followDTO!!.followers[currentUserUid!!] =true
+
+                transaction.set(tsDocFollower,followDTO!! )
+                return@runTransaction
+            }
+            if(followDTO!!.followers.containsKey(currentUserUid)){
+                //it cancel my follwer when i follow a third person
+                followDTO!!.followerCount = followDTO!!.followerCount -1
+                followDTO!!.followers.remove(currentUserUid!!)
+            }else {
+                //it add my follower when i don't follow a third person
+                followDTO!!.followerCount = followDTO!!.followerCount +1
+                followDTO!!.followers[currentUserUid!!] = true
+            }
+            transaction.set(tsDocFollower,followDTO!!)
+            return@runTransaction
+        }
     }
+    fun getProfileImage() {
+        firestore?.collection("profileImages")?.document(uid!!)?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+            if(documentSnapshot == null) return@addSnapshotListener
+            if(documentSnapshot.data != null) {
+                var url = documentSnapshot?.data!!["image"]
+               Glide.with(requireActivity()).load(url).apply(RequestOptions().circleCrop()).into(fragmentView?.account_iv_profile!!)
+        }
+
+        }
+    }
+
     inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
 
