febe75472f4a2bf6fca8a4e4a2ec437b4114dba9
JungMin LEE <leejungmin05@gmail.com>
2021-02-01 23:41:17 +0900

    Pull Request test

--- app/src/main/java/com/example/cloneinstagram/main/MainActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
