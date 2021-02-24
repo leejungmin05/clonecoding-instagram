466a4f815626c29382b3f4bb217b547fb041dbce
JungMin LEE <leejungmin05@gmail.com>
2021-02-25 02:27:00 +0900

    Add GridRecyclerAdapter

---
+++ app/src/main/java/com/example/cloneinstagram/main/grid/GridRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.cloneinstagram.main.grid
+
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.cloneinstagram.model.ContentDTO
+
+class GridRecyclerAdapter(
+    private var contentDTOs: ArrayList<ContentDTO>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val width = parent.context.resources.displayMetrics.widthPixels / 3
+
+            val imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) :
+            RecyclerView.ViewHolder(imageview)
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(
+                RequestOptions().centerCrop()
+            ).into(imageview)
+        }
+
+    }
