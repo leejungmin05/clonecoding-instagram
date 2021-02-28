491abbc2085ddf18cc86b90089fd50ddc658e95a
JungMin LEE <leejungmin05@gmail.com>
2021-02-28 22:20:18 +0900

    gridfragment

--- app/src/main/java/com/example/cloneinstagram/main/grid/GirdFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/main/grid/GirdFragment.kt
@@ -4,18 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.cloneinstagram.R
-import com.example.cloneinstagram.main.alarm.AlarmRecyclerAdapter
 import com.example.cloneinstagram.model.ContentDTO
 import com.example.cloneinstagram.model.FirebaseRepository
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 class GridFragment : Fragment() {
